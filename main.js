59171e714941d7d8977ae25a4b86771f8818ab39
rupin-1801 <rupinbhugra1411@gmail.com>
2021-05-19 13:06:51 +0530

    added initial js code

---
+++ main.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+function random(min, max){
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
